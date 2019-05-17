a43a2027cc7dac618db8e3a3625441515ffaca3c
Alper Hankendi <alper.hankendi@hepsiburada.com>
2019-05-17 21:07:14 +0300

    first commit

---
+++ review-with-checkpoint.py
@@ -0,0 +1,85 @@
+from __future__ import absolute_import, division, print_function
+
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+NUM_WORDS = 10000
+
+(train_data,train_labels),(test_data,test_labels) = keras.datasets.imdb.load_data(num_words=NUM_WORDS)
+
+def multi_hot_sequences(sequences, dimension):
+    results = np.zeros((len(sequences), dimension))
+    for i,word_indices in enumerate(sequences):
+        results[i,word_indices]= 1.0
+    return results
+
+
+train_data = multi_hot_sequences(train_data,dimension=NUM_WORDS)
+test_data  = multi_hot_sequences(test_data,dimension=NUM_WORDS)
+
+plt.plot(train_data[0])
+
+baseline_model = keras.Sequential([
+    # `input_shape` is only required here so that `.summary` works.
+    keras.layers.Dense(16, activation=tf.nn.relu, input_shape=(NUM_WORDS,)),
+    keras.layers.Dense(16, activation=tf.nn.relu),
+    keras.layers.Dense(1, activation=tf.nn.sigmoid)
+])
+
+baseline_model.compile(optimizer='adam',
+                       loss='binary_crossentropy',
+                       metrics=['accuracy', 'binary_crossentropy'])
+
+baseline_model.summary()
+
+baseline_history = baseline_model.fit(train_data,
+                                      train_labels,
+                                      epochs=20,
+                                      batch_size=512,
+                                      validation_data=(test_data, test_labels),
+                                      verbose=2)
+
+def plot_history(histories, key='binary_crossentropy'):
+    plt.figure(figsize=(16, 10))
+
+    for name, history in histories:
+        val = plt.plot(history.epoch, history.history['val_' + key],
+                       '--', label=name.title() + ' Val')
+        plt.plot(history.epoch, history.history[key], color=val[0].get_color(),
+                 label=name.title() + ' Train')
+
+    plt.xlabel('Epochs')
+    plt.ylabel(key.replace('_', ' ').title())
+    plt.legend()
+
+    plt.xlim([0, max(history.epoch)])
+
+
+#dropout
+dpt_model = keras.models.Sequential([
+    keras.layers.Dense(16, activation=tf.nn.relu, input_shape=(NUM_WORDS,)),
+    keras.layers.Dropout(0.5),
+    keras.layers.Dense(16, activation=tf.nn.relu),
+    keras.layers.Dropout(0.5),
+    keras.layers.Dense(1, activation=tf.nn.sigmoid)
+])
+
+dpt_model.compile(optimizer='adam',
+                  loss='binary_crossentropy',
+                  metrics=['accuracy','binary_crossentropy'])
+
+dpt_model_history = dpt_model.fit(train_data, train_labels,
+                                  epochs=20,
+                                  batch_size=512,
+                                  validation_data=(test_data, test_labels),
+                                  verbose=2)
+
+plot_history([('baseline', baseline_history),
+              ('dropout', dpt_model_history)])
+
+plt.show()
